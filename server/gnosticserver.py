b12edaa430c5d2f47129f4683adf708066652ba8
lucasg52 <141633910+lucasg52@users.noreply.github.com>
2025-11-22 17:17:04 -0600

    refactor

--- server/gnosticserver.py
+++ server/gnosticserver.py
@@ -1,14 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from .handler import posthandler
+from .handler import PostHandler
 
 class GnosticRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         try:
-            posthandler(self.rfile)
+            handler = PostHandler(self.rfile)
         except Exception as e:
             self.send_response_only(500, str(e))
         else:
             self.send_response_only(200, "Success")
+
+        print(handler.metadata)
 
 
 
--- server/handler.py
+++ server/handler.py
@@ -1,4 +1,11 @@
-from ..g_meta_scraper import scraper
+from ..meta_scarper import meta_scraper
 
-def posthandler(fp):
-    return scraper(fp)
+def handle(fp):
+    return PostHandler(fp)
+
+class PostHandler:
+    def __init__(self, fp):
+        self.metadata = meta_scraper(fp)
+        #self.body = 
+
+
