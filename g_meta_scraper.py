e62d23ed75cff054f0e79a917cd6bb6be6ab7fd9
RandomCrow 804 <jaspertheshark@gmail.com>
2025-11-22 16:31:11 -0600

    added access date, authors, keywords

--- g_meta_scraper.py
+++ g_meta_scraper.py
@@ -3,6 +3,12 @@ import datetime
 
 html_doc = """<html><head><title>The Dormouse's story</title></head>
 <body>
+<meta name="aaa" content="aaa">
+<meta name="author" content="authorname">
+<meta name="author" content="secondAuthor">
+<meta name="keywords" content="HTML, CSS, JavaScript">
+
+
 <p class="title"><b>The Dormouse's story</b></p>
 
 <p class="story">Once upon a time there were three little sisters; and their names were
@@ -17,10 +23,37 @@ and they lived at the bottom of a well.</p>
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 
+# testing
+# g_meta = soup.meta['name']    attribute stuff
+
+# title
 g_title = soup.title.string #this just works actually lol
+
+# author list
+g_author = []
+
+g_author_metas = soup.find_all("meta", attrs={"name": "author"})
+for meta in g_author_metas:
+    g_author.append(meta.get("content"))
+if len(g_author) == 0: g_author.append("None") #default author
+
+
+# keywords
+g_keywords = []
+
+g_keywords_metas = soup.find_all("meta", attrs={"name": "keywords"})
+for meta in g_keywords_metas:
+    g_keywords.append(meta.get("content"))
+if len(g_keywords) == 0: g_keywords.append("None") #default author
+
+
+# access date
 g_date_access = datetime.datetime.now()
 
 
 
 # debugging
+print(g_title)
+print(g_author)
+print(g_keywords)
 print(g_date_access)
