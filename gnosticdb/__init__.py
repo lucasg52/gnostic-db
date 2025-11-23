cad0e8316a14cc32f55f0f85680764fa73ce187a
lucasg52 <141633910+lucasg52@users.noreply.github.com>
2025-11-23 11:01:40 -0600

    added __main__ for module execution

--- gnosticdb/__init__.py
+++ gnosticdb/__init__.py
@@ -1,3 +1,30 @@
 """
 The gnostic database server
+
+flagz:
+-p <port>
+    Provide a port (default is 8000)
+--debug
+    Enable flask debug mode
 """
+import sys
+from .server.gnosticserver import app
+
+
+def main(args):
+    port = 8000
+    argiter = iter(args)
+    for arg in argiter:
+        if arg == "-p":
+            try:
+                port = next(argiter)
+            except StopIteration:
+                print("-p was not followed by a valid argument", file=sys.stderr)
+                pass
+    try:
+        port = int(port)
+    except ValueError:
+        print("-p was provided an invalid argument", file=sys.stderr)
+        port = 8000
+    app.run(debug = bool("--debug" in args), port=port, use_reloader=False)
+
